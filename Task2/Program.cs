275534c09e65e8c454e1be34ebcd1d45be4e9a75
user uaers <thatkrsn21@gmail.com>
2023-10-02 08:11:32 +0300

    Добавили задагие 1

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Результат: ");
+PrintNumbers(n);
+Console.ReadLine();
+
+void PrintNumbers(int n)
+{
+if (n <= 0)
+return;
+
+Console.Write(n + ", ");
+PrintNumbers(n - 1);
+}
